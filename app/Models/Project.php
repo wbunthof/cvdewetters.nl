5e1757cbbfe9302332a85147c9b2e0a97bc0b25a
Wouter Bunthof <wbunthof@gmail.com>
2022-10-15 17:51:20 +0200

    https://www.youtube.com/watch?v=W4ECg9ojLmQ&t=465s

---
+++ app/Models/Project.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use A17\Twill\Models\Behaviors\HasSlug;
+use A17\Twill\Models\Behaviors\HasMedias;
+use A17\Twill\Models\Model;
+
+class Project extends Model
+{
+    use HasSlug, HasMedias;
+
+    protected $fillable = [
+        'published',
+        'title',
+        'description',
+        'headline',
+        'url',
+    ];
+
+    public $slugAttributes = [
+        'title',
+    ];
+
+    public $mediasParams = [
+        'screengrab_desktop' => [
+            'default' => [
+                [
+                    'name' => 'default',
+                    'ratio' => 16 / 9,
+                ],
+            ],
+            'mobile' => [
+                [
+                    'name' => 'mobile',
+                    'ratio' => 1,
+                ],
+            ],
+            'flexible' => [
+                [
+                    'name' => 'free',
+                    'ratio' => 0,
+                ],
+                [
+                    'name' => 'landscape',
+                    'ratio' => 16 / 9,
+                ],
+                [
+                    'name' => 'portrait',
+                    'ratio' => 3 / 5,
+                ],
+            ],
+        ],
+    ];
+}
