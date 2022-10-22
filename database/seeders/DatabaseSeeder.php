b2964a29fb0b435c36439584ba7988443183722c
Wouter Bunthof <wbunthof@gmail.com>
2022-10-22 18:11:46 +0200

    Automatic insert a superadmin for wbunthof@gmail.com

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('twill_users')->insert([
+            'published' => 1,
+            'name' => 'Admin',
+            'email' => 'wbunthof@gmail.com',
+            'password' => '$2y$10$DUQS3nd8glNFAQUuX.o25.LL7aA4hEoceaRBH6gObKAMsUfgjHO5S',
+            'role' => 'SUPERADMIN',
+        ]);
     }
 }
