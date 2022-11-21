7c60b64bfbe8c6db0be3d03e94d5d5a8fd659e55
Wouter Bunthof <wbunthof@gmail.com>
2022-11-21 08:57:55 +0100

    Hoogheden update + verwijderen latijnse teksten

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
 
             <div class="section-title">
                 <h2>Hoogheden</h2>
-                <p>Dit zijn onze prachtige nieuwe hoogheden voor het seizoen 2022 - 2023!</p>
+                <p>Dit zijn onze prachtige nieuwe hoogheden voor 2022 - 2023!</p>
             </div>
 
             <div class="row">
@@ -16,15 +16,15 @@
                 </div>
                 <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
                     <h3>Lassie van Oranje</h3>
-                    <p class="fst-italic">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua.
-                    </p>
+{{--                    <p class="fst-italic">--}}
+{{--                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore--}}
+{{--                        magna aliqua.--}}
+{{--                    </p>--}}
                     <div class="row">
                         <div class="col-lg-6">
                             <ul>
                                 <li><i class="bi bi-chevron-right"></i> <strong>Naam:</strong> <span>Dennis Lassouw</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Leeftijd:</strong> <span>?</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Leeftijd:</strong> <span>42 Jaar</span></li>
                                 <li><i class="bi bi-chevron-right"></i> <strong>Geslacht:</strong> <span>Man</span></li>
                             </ul>
                         </div>
@@ -32,47 +32,82 @@
                         <div class="col-lg-6">
                             <ul>
                                 <li><i class="bi bi-chevron-right"></i> <strong>Woonplaats:</strong> <span>Nederwetten</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Gezin:</strong> <span>Vrouw en Twee Kinderen</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Werk:</strong> <span>?</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Gezin:</strong> <span>Laura - Kyara en Dante</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Werk:</strong> <span>Financial Risk Manager - APG</span></li>
                             </ul>
                         </div>
                     </div>
-                    <p>
-                        Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
-                        Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
-                    </p>
+{{--                    <p>--}}
+{{--                        Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.--}}
+{{--                        Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.--}}
+{{--                    </p>--}}
                 </div>
-                <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-right">
-                    <h3>Della Ross & d'n Rooie Duvel</h3>
-                    <p class="fst-italic">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua.
-                    </p>
+            </div>
+            <div class="row">
+                <div class="col-lg-10 pt-4 pt-lg-0 content" data-aos="fade-right">
+                    <h3>Della Ross</h3>
+{{--                    <p class="fst-italic">--}}
+{{--                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore--}}
+{{--                        magna aliqua.--}}
+{{--                    </p>--}}
                     <div class="row">
                         <div class="col-lg-6">
                             <ul>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Naam:</strong> <span>Maarten van Rossum</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Leeftijd:</strong> <span>53 Jaar</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Geslacht:</strong> <span>Man</span></li>
                             </ul>
                         </div>
+
                         <div class="col-lg-6">
                             <ul>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Woonplaats:</strong> <span>Nederwetten</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Gezin:</strong> <span>Janneke - Staf en Vos</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Werk:</strong> <span>Operational Manager - Maison van den Boer</span></li>
                             </ul>
                         </div>
                     </div>
-                    <p>
-                        Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
-                        Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
-                    </p>
+
+{{--                    <p>--}}
+{{--                        Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.--}}
+{{--                        Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.--}}
+{{--                    </p>--}}
                 </div>
-                <div class="col-lg-4" data-aos="fade-left">
-                    <img src="{{asset("img/placeholder.png")}}" class="img-fluid" alt="">
+                <div class="col-lg-2" data-aos="fade-left">
+                    <img src="{{asset("img/AvatarDellaRoss.jpg")}}" class="img-fluid" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-10 pt-4 pt-lg-0 content" data-aos="fade-right">
+                    <h3>d'n Rooie Duvel</h3>
+                    {{--                    <p class="fst-italic">--}}
+                    {{--                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore--}}
+                    {{--                        magna aliqua.--}}
+                    {{--                    </p>--}}
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Naam:</strong> <span>Piet Grauls</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Leeftijd:</strong> <span>41 Jaar</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Geslacht:</strong> <span>Man</span></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <ul>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Woonplaats:</strong> <span>Nederwetten</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Gezin:</strong> <span>Myke - Rosa en Suze</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Werk:</strong> <span>Portfolio Manager - RWE</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    {{--                    <p>--}}
+                    {{--                        Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.--}}
+                    {{--                        Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.--}}
+                    {{--                    </p>--}}
+                </div>
+                <div class="col-lg-2" data-aos="fade-left">
+                    <img src="{{asset("img/AvatarDnRooieDuvel.jpg")}}" class="img-fluid" alt="">
                 </div>
             </div>
 
@@ -86,11 +121,11 @@
 
                 <div class="section-title">
                     <h2>Programma</h2>
-                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+{{--                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>--}}
                 </div>
-                <h2>
-                    Receptieweekend
-                </h2>
+{{--                <h2>--}}
+{{--                    Receptieweekend--}}
+{{--                </h2>--}}
 
             <div class="row">
                 <div class="col-xl-6" >
@@ -98,11 +133,11 @@
                     <div class="resume-item pb-0">
                         <h4>Bonte avond</h4>
                         <h5>19.11 - 01.11</h5>
-                        <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
-                        <ul>
-                            <li>€15,-</li>
-                            <li><i>Link naar kopen tickets</i></li>
-                        </ul>
+                        <p><em>€15,-</em></p>
+{{--                        <ul>--}}
+{{--                            <li>€15,-</li>--}}
+{{--                            <li><i>Link naar kopen tickets</i></li>--}}
+{{--                        </ul>--}}
                     </div>
                 </div>
                 <div class="col-xl-6" >
@@ -121,9 +156,13 @@
 
             <br>
             <br>
-            <h2>
-                Carnaval
-            </h2>
+            <div class="section-title">
+                <h2></h2>
+                {{--                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>--}}
+            </div>
+{{--            <h2>--}}
+{{--                Carnaval--}}
+{{--            </h2>--}}
             <div class="row">
 
                 <div class="col-xxl-2" >
@@ -133,11 +172,11 @@
                     <div class="resume-item pb-0">
                         <h4>Bonte avond</h4>
                         <h5>19.11 - 00.11</h5>
-                        <p><em>Lorum Ipsum</em></p>
-                        <ul>
-                            <li>€15,-</li>
-                            <li><i>Link naar kopen tickets</i></li>
-                        </ul>
+                        <p><em>€15,-</em></p>
+{{--                        <ul>--}}
+{{--                            <li>€15,-</li>--}}
+{{--                            <li><i>Link naar kopen tickets</i></li>--}}
+{{--                        </ul>--}}
                     </div>
                 </div>
 
